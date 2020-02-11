1dca9046031f959e750981f49be2f14598bbbf01
Luke McNeil <lukemcneil61@gmail.com>
2020-02-10 23:24:13 -0500

    nothing

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
-import javax.swing.JComponent;
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class Main {
     public Main() {
-		GraphMaker gm = new GraphMaker();
-		JFrame frame = new JFrame();
-		frame.setSize(800, 600);
-		frame.setVisible(true);
-		frame.setFocusable(true);
-		JComponent mainComponent = new MainComponent(gm.getGraph());
-		frame.add(mainComponent);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.repaint();
+        GraphMaker gm = new GraphMaker();
+        JFrame frame = new JFrame();
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+        frame.setFocusable(true);
+        JComponent mainComponent = new MainComponent(gm.getGraph());
+        frame.add(mainComponent);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.repaint();
+		
     }
 
     public static void main(String[] args) {
