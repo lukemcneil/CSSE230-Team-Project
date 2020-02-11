9d038f1dc03ed0a8a19d3e03120d9ad84bd6316a
Luke McNeil <lukemcneil61@gmail.com>
2020-02-10 22:09:30 -0500

    added test case

--- src/GraphMaker.java
+++ src/GraphMaker.java
@@ -13,25 +13,46 @@ public class GraphMaker {
 
         Graph g = new Graph();
 
+        Node n0 = new Node("n0");
         Node n1 = new Node("n1");
         Node n2 = new Node("n2");
         Node n3 = new Node("n3");
         Node n4 = new Node("n4");
+        Node n5 = new Node("n5");
+        Node n6 = new Node("n6");
+        Node n7 = new Node("n7");
 
-        addEdge(n1, n2, 15);
-        addEdge(n1, n3, 5);
-        addEdge(n2, n3, 10);
-        addEdge(n3, n4, 20);
+        addEdge(n0, n1, 3);
+        addEdge(n0, n2, 5);
 
+        addEdge(n1, n2, 6);
+        addEdge(n1, n6, 1);
+
+        addEdge(n2, n5, 7);
+        addEdge(n2, n6, 7);
+
+        addEdge(n3, n7, 2);
+
+        addEdge(n4, n6, 7);
+        addEdge(n4, n7, 7);
+
+        addEdge(n5, n7, 1);
+
+        addEdge(n6, n7, 7);
+
+        g.nodes.put("n0", n0);
         g.nodes.put("n1", n1);
         g.nodes.put("n2", n2);
         g.nodes.put("n3", n3);
         g.nodes.put("n4", n4);
+        g.nodes.put("n5", n5);
+        g.nodes.put("n6", n6);
+        g.nodes.put("n7", n7);
         System.out.println(g + "\n\n");
 
-        write(g.nodes, "XML-data/test.xml");
+        write(g.nodes, "XML-data/nodes.xml");
 
-        Hashtable<String, Node> readNodes = read("XML-data/test.xml");
+        Hashtable<String, Node> readNodes = read("XML-data/nodes.xml");
         Graph newGraph = new Graph();
         newGraph.nodes = readNodes;
         System.out.println(newGraph);
--- src/Node.java
+++ src/Node.java
@@ -2,109 +2,109 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 
 public class Node {
-	public String name;
-	public ArrayList<Edge> edges;
-	public Hashtable<Node,Node> pathFinder;
+    public String name;
+    public ArrayList<Edge> edges;
+    public Hashtable<Node, Node> pathFinder;
 
-	public Node() {
-		this.edges= new ArrayList<Edge>();
-		pathFinder = new Hashtable<Node,Node>();
-	}
+    public Node() {
+        this.edges = new ArrayList<Edge>();
+        pathFinder = new Hashtable<Node, Node>();
+    }
 
-	public Node(String name) {
-		this.name = name;
-		this.edges = new ArrayList<Edge>();
-		pathFinder = new Hashtable<Node,Node>();
-	}
+    public Node(String name) {
+        this.name = name;
+        this.edges = new ArrayList<Edge>();
+        pathFinder = new Hashtable<Node, Node>();
+    }
 
-	public ArrayList<Edge> getEdges() {
-		return edges;
-	}
-	
-	public void addEdge(Node finish, int cost) {
-		edges.add(new Edge(finish,cost));
-	}
+    public ArrayList<Edge> getEdges() {
+        return edges;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void addEdge(Node finish, int cost) {
+        edges.add(new Edge(finish, cost));
+    }
 
-	public void setEdges(ArrayList<Edge> edges) {
-		this.edges.addAll(edges);
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public ArrayList<Node> pathTo(Node destination){
-		if(pathFinder.isEmpty()) {
-			this.dijkstra();
-		}
-		ArrayList<Node> finalPath = new ArrayList<Node>();
-		finalPath.add(destination);
-		Node temp = pathFinder.get(destination);
-		while(temp!=null) {
-			finalPath.add(0,temp);
-			temp = pathFinder.get(temp);
-		}
-		return finalPath;
-		
-	}
+    public void setEdges(ArrayList<Edge> edges) {
+        this.edges.addAll(edges);
+    }
 
-	public Hashtable<Node, Integer> dijkstra() {
-		Hashtable<Node, Integer> costTable = new Hashtable<Node, Integer>();
-		ArrayList<Node> completed = new ArrayList<Node>();
+    public void setName(String name) {
+        this.name = name;
+    }
 
-		this.dijkstraHelper(costTable, completed,pathFinder);
+    public ArrayList<Node> pathTo(Node destination) {
+        if (pathFinder.isEmpty()) {
+            this.dijkstra();
+        }
+        ArrayList<Node> finalPath = new ArrayList<Node>();
+        finalPath.add(destination);
+        Node temp = pathFinder.get(destination);
+        while (temp != null) {
+            finalPath.add(0, temp);
+            temp = pathFinder.get(temp);
+        }
+        return finalPath;
 
-		return costTable;
-	}
+    }
 
-	private void dijkstraHelper(Hashtable<Node, Integer> costTable, ArrayList<Node> completed,Hashtable<Node,Node> path) {
-		completed.add(this);
-		for (Edge curr : this.edges) {
-			if (!completed.contains(curr.end)) {
-				int currCost = costTable.get(this)==null ? 0 : costTable.get(this);
-				if (costTable.containsKey(curr.end)) {
-					int temp = curr.cost + costTable.get(this);
-					if (temp < costTable.get(curr.end)) {
-						costTable.replace(curr.end, curr.cost + currCost);
-						path.put(curr.end,this);
-					}
-				} else {
-					costTable.put(curr.end, curr.cost + currCost);
-					path.put(curr.end, this);
-				}
+    public Hashtable<Node, Integer> dijkstra() {
+        Hashtable<Node, Integer> costTable = new Hashtable<Node, Integer>();
+        ArrayList<Node> completed = new ArrayList<Node>();
 
-			}
+        this.dijkstraHelper(costTable, completed, pathFinder);
+        costTable.put(this, 0);
+        return costTable;
+    }
 
-		}
-		Node temp = minNodeInMap(costTable, completed);
-		if (temp != null) {
-			temp.dijkstraHelper(costTable, completed,path);
-		}
-	}
+    private void dijkstraHelper(Hashtable<Node, Integer> costTable, ArrayList<Node> completed, Hashtable<Node, Node> path) {
+        completed.add(this);
+        for (Edge curr : this.edges) {
+            if (!completed.contains(curr.end)) {
+                int currCost = costTable.get(this) == null ? 0 : costTable.get(this);
+                if (costTable.containsKey(curr.end)) {
+                    int temp = curr.cost + costTable.get(this);
+                    if (temp < costTable.get(curr.end)) {
+                        costTable.replace(curr.end, curr.cost + currCost);
+                        path.put(curr.end, this);
+                    }
+                } else {
+                    costTable.put(curr.end, curr.cost + currCost);
+                    path.put(curr.end, this);
+                }
 
-	private Node minNodeInMap(Hashtable<Node, Integer> costTable, ArrayList<Node> completed) {
-		int minCost = 10000000;
-		Node minNode = null;
-		for (Node n : costTable.keySet()) {
-			if (costTable.get(n) < minCost && costTable.get(n) != 0 && !completed.contains(n)) {
-				minCost = costTable.get(n);
-				minNode = n;
-			}
-		}
-		return minNode;
-	}
+            }
 
-	public String toString() {
-		String result = "name: " + name;
-		result += " | edges: ";
-		for (Edge e : edges) {
-			result += e.toString();
-		}
-		result += "\n";
-		return result;
-	}
+        }
+        Node temp = minNodeInMap(costTable, completed);
+        if (temp != null) {
+            temp.dijkstraHelper(costTable, completed, path);
+        }
+    }
+
+    private Node minNodeInMap(Hashtable<Node, Integer> costTable, ArrayList<Node> completed) {
+        int minCost = 10000000;
+        Node minNode = null;
+        for (Node n : costTable.keySet()) {
+            if (costTable.get(n) < minCost && costTable.get(n) != 0 && !completed.contains(n)) {
+                minCost = costTable.get(n);
+                minNode = n;
+            }
+        }
+        return minNode;
+    }
+
+    public String toString() {
+        String result = "name: " + name;
+        result += " | edges: ";
+        for (Edge e : edges) {
+            result += e.toString();
+        }
+        result += "\n";
+        return result;
+    }
 }
