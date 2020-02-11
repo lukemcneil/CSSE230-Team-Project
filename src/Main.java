46184c9276c7f2c2a560620e234142f10884e4b9
ansarij <ansarij@137.112.233.168>
2020-02-11 13:47:34 -0500

    Failure

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
-import javax.swing.*;
+import java.awt.BorderLayout;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,7 +12,12 @@ public class Main {
         frame.setVisible(true);
         frame.setFocusable(true);
         JComponent mainComponent = new MainComponent(gm.getGraph());
-        frame.add(mainComponent);
+        JPanel mainPanel = new JPanel();
+        mainPanel.add(mainComponent);
+        JPanel buttonPanel = new ButtonPanel();
+        frame.add(mainPanel, BorderLayout.CENTER);
+        frame.add(buttonPanel, BorderLayout.EAST);
+//        frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.repaint();
     }
