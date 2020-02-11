c81ac019faff65ec0f8e6f061a8cae516336e9aa
Luke McNeil <lukemcneil61@gmail.com>
2020-02-10 23:15:48 -0500

    draws nodes and edges

--- src/GraphMaker.java
+++ src/GraphMaker.java
@@ -8,51 +8,53 @@ import java.util.Hashtable;
 
 public class GraphMaker {
 
-	private Graph g;
-	
+    private Graph g;
+
     public Graph getGraph() {
-		return g;
-	}
+        return g;
+    }
 
     public GraphMaker() {
-    	 g = new Graph();
+        g = new Graph();
 
-         Node n0 = new Node("n0");
-         Node n1 = new Node("n1");
-         Node n2 = new Node("n2");
-         Node n3 = new Node("n3");
-         Node n4 = new Node("n4");
-         Node n5 = new Node("n5");
-         Node n6 = new Node("n6");
-         Node n7 = new Node("n7");
+        int xwidth = 800;
+        int ywidth = 600;
+        Node n0 = new Node("n0", 100, 100);
+        Node n1 = new Node("n1", 200, 100);
+        Node n2 = new Node("n2", 300, 100);
+        Node n3 = new Node("n3", 100, 200);
+        Node n4 = new Node("n4", 200, 300);
+        Node n5 = new Node("n5", 300, 400);
+        Node n6 = new Node("n6", 400, 300);
+        Node n7 = new Node("n7", 500, 100);
 
-         addEdge(n0, n1, 3);
-         addEdge(n0, n2, 5);
+        addEdge(n0, n1, 3);
+        addEdge(n0, n2, 5);
 
-         addEdge(n1, n2, 6);
-         addEdge(n1, n6, 1);
+        addEdge(n1, n2, 6);
+        addEdge(n1, n6, 1);
 
-         addEdge(n2, n5, 7);
-         addEdge(n2, n6, 7);
+        addEdge(n2, n5, 7);
+        addEdge(n2, n6, 7);
 
-         addEdge(n3, n7, 2);
+        addEdge(n3, n7, 2);
 
-         addEdge(n4, n6, 7);
-         addEdge(n4, n7, 7);
+        addEdge(n4, n6, 7);
+        addEdge(n4, n7, 7);
 
-         addEdge(n5, n7, 1);
+        addEdge(n5, n7, 1);
 
-         addEdge(n6, n7, 7);
+        addEdge(n6, n7, 7);
 
-         g.nodes.put("n0", n0);
-         g.nodes.put("n1", n1);
-         g.nodes.put("n2", n2);
-         g.nodes.put("n3", n3);
-         g.nodes.put("n4", n4);
-         g.nodes.put("n5", n5);
-         g.nodes.put("n6", n6);
-         g.nodes.put("n7", n7);
-         System.out.println(g + "\n\n");
+        g.nodes.put("n0", n0);
+        g.nodes.put("n1", n1);
+        g.nodes.put("n2", n2);
+        g.nodes.put("n3", n3);
+        g.nodes.put("n4", n4);
+        g.nodes.put("n5", n5);
+        g.nodes.put("n6", n6);
+        g.nodes.put("n7", n7);
+        System.out.println(g + "\n\n");
 
 //         try {
 //			write(g.nodes, "XML-data/nodes.xml");
@@ -66,8 +68,8 @@ public class GraphMaker {
 //		}
 
     }
-    
-	public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws Exception {
 
 //        g = new Graph();
 //
--- src/MainComponent.java
+++ src/MainComponent.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
-import java.util.Random;
 
 public class MainComponent extends JComponent {
 
@@ -13,22 +12,22 @@ public class MainComponent extends JComponent {
     }
 
     public void paintComponent(Graphics g) {
-    	ArrayList<NodeComponent> nodes=new ArrayList<NodeComponent>();
-    	ArrayList<EdgeComponent> owTheEdge=new ArrayList<EdgeComponent>();
+        ArrayList<NodeComponent> nodes = new ArrayList<NodeComponent>();
+        ArrayList<EdgeComponent> owTheEdge = new ArrayList<EdgeComponent>();
         for (String i : graph.getNodes().keySet()) {
 //			g.drawOval(graph.getNodes().get(i).getVisualX(), graph.getNodes().get(i).getVisualY(), 30, 30);
-        	nodes.add(new NodeComponent(graph.getNodes().get(i)));
+            nodes.add(new NodeComponent(graph.getNodes().get(i)));
         }
-        for(NodeComponent n: nodes) {
-        	n.drawOn(g);
-        	for(Edge e: n.target.getEdges()) {
-        		Node n1=e.end;
-        		Node n2=n.target;
-        		owTheEdge.add(new EdgeComponent(n1.x, n2.x, n1.y, n2.y, e.cost));
-        	}
+        for (NodeComponent n : nodes) {
+            n.drawOn(g);
+            for (Edge e : n.target.getEdges()) {
+                Node n1 = e.end;
+                Node n2 = n.target;
+                owTheEdge.add(new EdgeComponent(e.cost, n1.x, n2.x, n1.y, n2.y));
+            }
         }
-        for(EdgeComponent e: owTheEdge) {
-        	e.drawOn(g);
+        for (EdgeComponent e : owTheEdge) {
+            e.drawOn(g);
         }
-    } 
+    }
 }
