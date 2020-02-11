be6d406e6045a335c37f444f8698d71a5ebe7c93
Luke McNeil <lukemcneil61@gmail.com>
2020-02-10 22:20:18 -0500

    maybe working

--- src/MainComponent.java
+++ src/MainComponent.java
@@ -1,19 +1,18 @@
-import java.awt.Graphics;
-
-import javax.swing.JComponent;
+import javax.swing.*;
+import java.awt.*;
 
 public class MainComponent extends JComponent {
-	
-	private Graph graph;
-	
-	public MainComponent(Graph g) {
-		this.graph = g;
-		repaint();
-	}
-	
-	public void paintComponent(Graphics g) {
-		for(String i : graph.getNodes().keySet()) {
-			g.drawOval(graph.getNodes().get(i).getVisualX(), graph.getNodes().get(i).getVisualY(), 30, 30);
-		}
-	}
+
+    private Graph graph;
+
+    public MainComponent(Graph g) {
+        this.graph = g;
+        repaint();
+    }
+
+    public void paintComponent(Graphics g) {
+        for (String i : graph.getNodes().keySet()) {
+//			g.drawOval(graph.getNodes().get(i).getVisualX(), graph.getNodes().get(i).getVisualY(), 30, 30);
+        }
+    }
 }
