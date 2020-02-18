d21aff91d2669a1f89939585c47a271d11261ec3
Luke McNeil <lukemcneil61@gmail.com>
2020-02-18 18:39:47 -0500

    Auto stash before merge of "master" and "origin/master"

--- src/Listeners/ModeListener.java
+++ src/Listeners/ModeListener.java
@@ -5,14 +5,15 @@ import java.awt.event.ActionListener;
 
 import Graph.Graph;
 
-public class ModeListener implements ActionListener{
+public class ModeListener implements ActionListener {
 	private Graph g;
 	private FindPathListener f;
-	
-	public ModeListener(Graph g,FindPathListener f) {
+
+	public ModeListener(Graph g, FindPathListener f) {
 		this.g = g;
 		this.f = f;
 	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		g.switchMode();
